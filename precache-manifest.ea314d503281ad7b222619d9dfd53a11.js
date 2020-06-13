7ecdab40c8bedfac69de5c55e21327fdc07e2866
Ahmad <13ahmad.hm78@gmail.com>
2020-06-14 03:40:56 +0430

    1st Commit

---
+++ precache-manifest.ea314d503281ad7b222619d9dfd53a11.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8063f87e973a00dc0884214837824be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1761b1e6c75fd0f66e1",
+    "url": "/static/css/2.2be2a64a.chunk.css"
+  },
+  {
+    "revision": "00441baa04638c2bc67c",
+    "url": "/static/css/main.ac0ee3d1.chunk.css"
+  },
+  {
+    "revision": "c1761b1e6c75fd0f66e1",
+    "url": "/static/js/2.26ca7b03.chunk.js"
+  },
+  {
+    "revision": "5c639e0eadf54404f50f772674630a25",
+    "url": "/static/js/2.26ca7b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00441baa04638c2bc67c",
+    "url": "/static/js/main.7b4fe395.chunk.js"
+  },
+  {
+    "revision": "69de1eb5dabe24cc3b12",
+    "url": "/static/js/runtime-main.68fc14a2.js"
+  },
+  {
+    "revision": "5fe8dc747e7f1dea6f746fed88b017a0",
+    "url": "/static/media/Imagine Dragons.5fe8dc74.jpg"
+  },
+  {
+    "revision": "cb2e9011c914d652a38df88e77c1acf1",
+    "url": "/static/media/billie.cb2e9011.jpg"
+  },
+  {
+    "revision": "51fb7045ab90a634ec36a346b1c2c509",
+    "url": "/static/media/camila.51fb7045.png"
+  },
+  {
+    "revision": "b4b569df78581adf2c6208c76c306b3d",
+    "url": "/static/media/closer.b4b569df.jpg"
+  },
+  {
+    "revision": "473318b168e1ebb1bd38bd5c050b960f",
+    "url": "/static/media/eminem.473318b1.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "42750bf9958cf6904498246a7cf4b328",
+    "url": "/static/media/havana.42750bf9.jpg"
+  },
+  {
+    "revision": "f439d96b82e0b1405a081b65e16159ef",
+    "url": "/static/media/lady gaga.f439d96b.jpg"
+  },
+  {
+    "revision": "6caf93917aa09194c43356393c881ab6",
+    "url": "/static/media/profile.6caf9391.jpg"
+  },
+  {
+    "revision": "fe8e465d05102b57dea20408635eab70",
+    "url": "/static/media/radioactive.fe8e465d.jpg"
+  },
+  {
+    "revision": "4ba1a3a3826eab17317023d03c35ac99",
+    "url": "/static/media/torn.4ba1a3a3.png"
+  },
+  {
+    "revision": "4d0305ee22ebf0f08139142113c0cd4f",
+    "url": "/static/media/travis.4d0305ee.jpg"
+  },
+  {
+    "revision": "404a502f65101413a5c97c3e5743ed17",
+    "url": "/static/media/weeknd.404a502f.jpg"
+  }
+]);
